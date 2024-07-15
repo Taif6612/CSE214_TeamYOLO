fcbd06ef3ce9defb06edc5c57d83f6c62be3d81e
Taif6612 <taifurrahman66123@gmail.com>
2024-07-15 21:52:26 +0600

    Revert "Start of the Project"
    This reverts commit 926b51bb9d96d42f45c59e6bb198473d10dd9bf5.

--- WelfareAssociation/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module oop.welfareassociation.welfareassociation {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens oop.welfareassociation.welfareassociation to javafx.fxml;
-    exports oop.welfareassociation.welfareassociation;
-}
