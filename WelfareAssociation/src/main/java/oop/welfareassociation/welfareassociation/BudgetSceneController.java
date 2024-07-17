44896b3fcd4556964f8cc8c611bfc7f58af361b9
Fairooz29 <fairoozprapti29@gmail.com>
2024-07-17 21:45:45 +0600

    Event Logistic Scene fxml
    Codes for buttons are added

--- WelfareAssociation/src/main/java/oop/welfareassociation/welfareassociation/BudgetSceneController.java
+++
@@ -1,7 +0,0 @@
-package oop.welfareassociation.welfareassociation;
-
-public class BudgetSceneController
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
--- WelfareAssociation/src/main/java/oop/welfareassociation/welfareassociation/DashBoardSceneController.java
+++ WelfareAssociation/src/main/java/oop/welfareassociation/welfareassociation/DashBoardSceneController.java
@@ -10,38 +10,51 @@ public class DashBoardSceneController
 
     @javafx.fxml.FXML
     public void eventSafetyAndComplianceMouseOnClick(ActionEvent actionEvent) {
-        // Code to nevigate to the Safety and Compliance Screen
-        System.out.println("Navigating to Event Safety and Compliance");
+
+        loadScene("eventSafety.fxml");
+
     }
 
     @javafx.fxml.FXML
     public void eventPromotionMouseOnClick(ActionEvent actionEvent) {
-        System.out.println("Navigating to Event Promotion");
+        loadScene("eventSafety.fxml");
+
     }
 
     @javafx.fxml.FXML
     public void postEventEvalutionMouseOnClick(ActionEvent actionEvent) {
-        System.out.println("Navigating to Post Event Evalution");
+        loadScene("eventSafety.fxml");
     }
 
     @javafx.fxml.FXML
     public void eventLogisticsMouseOnClick(ActionEvent actionEvent) {
-        System.out.println("Navigating to eventLogistics");
+        loadScene("eventLogisticsScene.fxml");
+
     }
 
     @javafx.fxml.FXML
     public void eventBudgetMouseOnClick(ActionEvent actionEvent) {
+        loadScene("eventSafety.fxml");
+
     }
 
     @javafx.fxml.FXML
     public void eventPlanningMouseOnClick(ActionEvent actionEvent) {
+        loadScene("eventPlanningScene.fxml");
+
     }
 
     @javafx.fxml.FXML
     public void eventRegistrationsMouseOnClick(ActionEvent actionEvent) {
+        loadScene("eventSafety.fxml");
+
     }
 
     @javafx.fxml.FXML
     public void stakeHolderCoordinationMouseOnClick(ActionEvent actionEvent) {
+        loadScene("eventSafety.fxml");
+
     }
+
+
 }
