16ee495c0a14cf0d35364d4f904073c80e94a86d
Taif6612 <taifurrahman66123@gmail.com>
2024-07-17 18:56:12 +0600

    Changed some base files to look more organised

---
+++ WelfareAssociation/src/main/java/oop/welfareassociation/welfareassociation/StartupLgLoController.java
@@ -0,0 +1,18 @@
+package oop.welfareassociation.welfareassociation;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class StartupLgLoController
+{
+    @javafx.fxml.FXML
+    private Label welcomeText;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void onHelloButtonClick(ActionEvent actionEvent) {
+    }
+}
