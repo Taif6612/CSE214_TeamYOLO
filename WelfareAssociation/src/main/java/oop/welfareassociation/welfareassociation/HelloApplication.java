c8f8c7e97f49584e713ca18f059420f6c45757bf
Fairooz29 <fairoozprapti29@gmail.com>
2024-07-18 00:02:41 +0600

    eventBudgetController
    deleted the initialize codes and button codes

--- WelfareAssociation/src/main/java/oop/welfareassociation/welfareassociation/HelloApplication.java
+++ WelfareAssociation/src/main/java/oop/welfareassociation/welfareassociation/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("eventPlanningScene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("eventBudgetScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
--- WelfareAssociation/src/main/java/oop/welfareassociation/welfareassociation/eventBudgetSceneController.java
+++ WelfareAssociation/src/main/java/oop/welfareassociation/welfareassociation/eventBudgetSceneController.java
@@ -7,35 +7,28 @@ import javafx.scene.control.TextField;
 
 import static javafx.collections.FXCollections.observableArrayList;
 
-public class eventBudgetSceneController
-{
+public class eventBudgetSceneController {
     @javafx.fxml.FXML
-    private TableColumn <Expense, String>budgetCategoryColumn;
+    private TableColumn<Expense, String> budgetCategoryColumn;
     @javafx.fxml.FXML
     private TextField budgetCategoryTextField;
     @javafx.fxml.FXML
     private TableView<Expense> expenseTableView;
     @javafx.fxml.FXML
-    private TableColumn <Expense, Double>budgetAmountColumn;
+    private TableColumn<Expense, Double> budgetAmountColumn;
     @javafx.fxml.FXML
     private TextField budgetAmountTextField;
 
     @javafx.fxml.FXML
     public void initialize() {
-        expenses =observableArrayList();
-        budgetAmountColumn.setCellFactory();
-        budgetCategoryColumn.setCellFactory();
+        //
 
 
     }
 
     @javafx.fxml.FXML
     public void addExpenseButton(ActionEvent actionEvent) {
-        String category = budgetCategoryTextField.getText();
-        Double amount = Double.parseDouble(budgetAmountTextField.getText());
+        //
 
-        Expense expense = new Expense(category, amount);
-        Expense.add(expense);
-
-
+    }
 }
