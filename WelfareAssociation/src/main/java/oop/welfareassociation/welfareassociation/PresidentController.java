714ac8be8a5d84d28f6c81906f8da100535f3dd1
Taif6612 <taifurrahman66123@gmail.com>
2024-07-17 22:45:21 +0600

    Added President Controller as well as fxml file

--- WelfareAssociation/src/main/java/oop/welfareassociation/welfareassociation/PresidentController.java
+++ WelfareAssociation/src/main/java/oop/welfareassociation/welfareassociation/PresidentController.java
@@ -4,4 +4,9 @@ public class PresidentController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void reviewWelfareProgramsOnActionButton(ActionEvent actionEvent) {
+    }
+}
